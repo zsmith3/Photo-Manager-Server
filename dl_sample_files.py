a25b03260dcb2be6ae610ac2da9dbd37d2921c0e
zsmith3 <zsmith3.dev@gmail.com>
2019-01-28 19:37:35 +0000

    Heroku - fix issue with release phase python

--- dl_sample_files.py
+++ dl_sample_files.py
@@ -3,6 +3,6 @@ import sys
 print(f"TEST CONTENT, arg={sys.argv[0]}")
 
 # Test persistence of file
-f = open("test.txt")
+f = open("test.txt", "w")
 f.write(f"TEST CONTENT, arg={sys.argv[0]}")
 f.close()
