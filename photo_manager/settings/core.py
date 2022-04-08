e85170f698b020b371c7375d6e49f3c5c8ee6e12
zsmith3 <zsmith3.dev@gmail.com>
2022-04-08 12:34:11 +0100

    Add production logging

--- photo_manager/settings/core.py
+++ photo_manager/settings/core.py
@@ -96,3 +96,57 @@ JWT_AUTH = {"JWT_EXPIRATION_DELTA": datetime.timedelta(365)}
 CORS_ORIGIN_WHITELIST = ("https://localhost", "http://localhost:1234")
 
 PYTHON_LOG_MAX_LINES = 100000
+
+# Add production logging
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
+		},
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
+		},
+	},
+	'formatters': {
+		'django.server': {
+			'()': 'django.utils.log.ServerFormatter',
+			'format': '[%(server_time)s] %(message)s',
+		}
+	},
+	'handlers': {
+		'console': {
+			'level': 'INFO',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'console_debug_false': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'logging.FileHandler',
+			'filename': os.path.join(BASE_DIR, 'logs/errors.log'),
+		},
+		'django.server': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+			'formatter': 'django.server',
+		},
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console', 'console_debug_false', 'mail_admins'],
+			'level': 'INFO',
+		},
+		'django.server': {
+			'handlers': ['django.server'],
+			'level': 'INFO',
+			'propagate': False,
+		}
+	}
+}
